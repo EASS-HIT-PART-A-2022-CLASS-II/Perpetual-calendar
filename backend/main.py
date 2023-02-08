6541f0f87f39812bdc12ea26e4a0c4c847bc3dc4
Dudi Kreis <dudikreis@gmail.com>
2023-02-08 17:20:01 +0200

    update

--- backend/main.py
+++ backend/main.py
@@ -3,28 +3,47 @@ from models import Event
 
 app = FastAPI()
 
-make_event = Event(title="Make FastAPI", description="Make a FastAPI app",
-                   start_time="2021-12-01 09:00", end_time="2021-12-01 10:00")
+# @app.post("/events/")
+# def create_event(event: Event):
+#     events_list.append(event)
+#     return {"event": event}
 
-events_list = []
 
+# @app.get("/show-events/")
+# def read_events():
+#     return {"events": events_list}
+
+
+# @app.put("/update/{event_id}")
+# def update_event(event_id: int, event: Event):
+#     return {"event_id": event_id, "event": event}
+
+
+# @app.delete("/delete/{event_id}")
+# def delete_event(event_id: int):
+#     return {"event_id": event_id}
+
+events = []
 
 @app.post("/events/")
-def create_event(event: Event):
-    events_list.append(event)
-    return {"event": event}
-
+async def create_event(event: Event):
+    # Save event to events list
+    events.append(event.dict())
+    return {"message": "Event created"}
 
 @app.get("/show-events/")
-def read_events():
-    return {"events": events_list}
-
+async def show_all_events():
+    # Return all events stored in events list
+    return {"events": events}
 
 @app.put("/update/{event_id}")
-def update_event(event_id: int, event: Event):
-    return {"event_id": event_id, "event": event}
-
+async def update_event(event_id: int, event: Event):
+    # Update event in events list with the given event_id
+    events[event_id] = event.dict()
+    return {"message": "Event updated"}
 
 @app.delete("/delete/{event_id}")
-def delete_event(event_id: int):
-    return {"event_id": event_id}
+async def delete_event(event_id: int):
+    # Delete event from events list with the given event_id
+    events.pop(event_id)
+    return {"message": "Event deleted"}
--- frontend/main.py
+++ frontend/main.py
@@ -1,63 +1,52 @@
 import streamlit as st
 import requests
 import datetime
-
-# st.title("Event Calendar")
 st.set_page_config(page_title="Personal Calendar",
                    page_icon=":calendar:", layout="wide")
 
 st.markdown(
     """
-    <h1 style='color: #4169E1; text-align: center; font-size: 50px; font-weight: bold; text-shadow: 2px 2px 4px #000000;'>
-    Personal Calendar
-    </h1>
-    <h2 style='color: #F0F8FF; text-align: center; font-size: 30px; font-weight: bold; text-shadow: 2px 2px 4px #000000;'>
-    Create and manage your events
-    </h2>
+    <body style='background-color: #FAEBD7;'>
+        <h1 style='color: #4169E1; text-align: center; font-size: 50px; font-weight: bold; text-shadow: 2px 2px 4px #000000;'>
+        Personal Calendar
+        </h1>
+        <h2 style='color: #4169E2; text-align: center; font-size: 30px; text-shadow: 2px 2px 4px #000000;'>
+        Create and manage your events
+        </h2>
+    </body>
     """,
     unsafe_allow_html=True,
 )
 
-# Create event
-title = st.text_input("Event Title", key='title',)
-description = st.text_input("Event Description", key='description')
-event_start = st.date_input("Event Start Time").isoformat()
-event_end = st.date_input("Event End Time").isoformat()
-start_hour = st.time_input("Event Start Hour").isoformat()
-end_hour = st.time_input("Event End Hour").isoformat()
+# Create a function to create an event
+def create_event(event):
+    r = requests.post("http://backend:8080/events/", json=event)
+    return r.json()
 
-event = {"title": title, "description": description,
-         "event_start": event_start, "event_end": event_end, "start_hour": start_hour, "end_hour": end_hour}
-
-# Send a POST request to the backend to create the event
-r = requests.post("http://backend:8080/events/", json={"event": event})
-
-# Handle the response
-if r.status_code == 200:
-    st.success("Event created successfully.")
-else:
-    st.error("Failed to create event.")
-
-
-# Read events
-if st.text_input("Show Events") == "show":
-    # Send a GET request to the backend to retrieve the events
+# Create a function to show all events
+def show_all_events():
     r = requests.get("http://backend:8080/show-events/")
+    return r.json()
 
-    events = r.json()["events"]
-    st.write("Events:")
-    for event in events:
-        st.write(event)
+def main():
+    st.sidebar.title("Personal Calendar")
+    page = st.sidebar.selectbox("Select a page", ["Add Event", "Show Events"])
 
-if st.button("Save Event"):
-    if (r.status_code == 200):
-        st.success("Event saved successfully")
-        event_title = st.text_input("Event Title", key='title1',)
-        event_description = st.text_input(
-            "Event Description", key='description1')
-        event_start_date = st.date_input("Event Start Date", key='start_date')
-        event_end_date = st.date_input("Event End Date", key='end_date')
-        event_hour = st.time_input("Event Hour", key='hour')
-        pass
+    if page == "Add Event":
+        st.title("Add Event")
+        title = st.text_input("Title", key='title')
+        description = st.text_input("Description", key='description')
+        start_time = st.date_input("Event Start Time").isoformat()
+        end_time = st.date_input("Event End Time").isoformat()
+        if st.button("Save Event"):
+            event = {"title": title, "description": description, "start_time": start_time, "end_time": end_time}
+            result = create_event(event)
+            st.success(result["message"])
     else:
-        st.error("Failed to save event")
+        st.title("Show Events")
+        events = show_all_events()
+        st.write("All events:")
+        st.write(events)
+
+if __name__ == "__main__":
+    main()
